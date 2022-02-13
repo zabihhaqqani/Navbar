8b453c9d9222e59e784ec1e407607031e2ffd087
Zabih <mdzabihhaqqani@gmail.com>
2022-02-13 16:12:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+// selecting button and links
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+//adding and removing classes using add and remove
+// navToggle.addEventListener("click",function(){
+    
+//     if(links.classList.contains("show-links")){
+//         links.classList.remove("show-links")
+//     }
+//     else{
+//         links.classList.add("show-links")
+//     }
+// })
+
+// add and remove classes - toggle
+
+navToggle.addEventListener("click",function(){
+    links.classList.toggle("show-links")
+})
